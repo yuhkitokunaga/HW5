9ae6cecc006197a71a397d8b5e8b79d4407dbc84
yuhkitokunaga <tokunaga@wharton.upenn.edu>
2025-11-05 22:53:36 -0500

    Add Starter Code Files

---
+++ src/SpellChecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SpellChecker {
+    // Use this field everytime you need to read user input
+    private Scanner inputReader; // DO NOT MODIFY
+
+    public SpellChecker() {
+      inputReader = new Scanner(System.in); // DO NOT MODIFY - must be included in this method
+      // TODO: Complete the body of this constructor, as necessary.
+    }
+  
+    public void start() {
+      // TODO: Complete the body of this method, as necessary.
+      inputReader.close();  // DO NOT MODIFY - must be the last line of this method!
+    }
+  
+    // You can of course write other methods as well.
+  }
---
+++ src/SpellCheckerRunner.java
@@ -0,0 +1,7 @@
+// DO NOT MODIFY THIS FILE AT ALL
+public class SpellCheckerRunner {
+    public static void main(String[] args) {
+      SpellChecker checker = new SpellChecker();
+      checker.start();
+    }
+  }
