73d7989890d421c217787f78000259dc78a7278e
sydwig-cpu <sydwig@sas.upenn.edu>
2025-11-11 10:03:11 -0500

    similarityyyy

--- src/WordRecommender.java
+++ src/WordRecommender.java
@@ -2,54 +2,84 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class WordRecommender {
     ArrayList<String> dictionaryContent;
 
-    public WordRecommender(String dictionaryFile) {
+    public WordRecommender(String dictionaryFile) throws FileNotFoundException {
         dictionaryContent = new ArrayList<>();
         FileInputStream input = new FileInputStream(dictionaryFile);
         Scanner sc = new Scanner(input);
         while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            dictionaryContent.add(line);
+            String line = sc.nextLine().trim();
+            if (!line.isEmpty()) {
+                dictionaryContent.add(line);
+            }
         }
         sc.close();
-
     }
-
-
-    // final String shortest = List.of("hello", "yes", "no")
-    //      .stream()
-    //       .min(Comparator.comparing(String::length))
-    //      .get();
-
-//          for (int i = 0; i < minLength)
-
     // Calculate IF %  of characters in common are at least 50%
     // similarity should be close to .length of word, NVM
 
 
-    public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
-        HashSet<String> setA = new HashSet<>();
-        for (String i : setA) {
-            setA.add(i);
-        }
-        HashSet<String> setB = new HashSet<>();
-        for (String i : setB) {
-            setB.add(i);
-        }
-        tolerance = setA.size() - setB.size();
-        commonPercent = setA.size() /setB.size();
+    public ArrayList<String> getWordSuggestions(String word,
+                                                int tolerance,
+                                                double commonPercent,
+                                                int topN) {
+        ArrayList<String> bestSuggest = new ArrayList<>();
+        ArrayList<Double> similarNum = new ArrayList<>();
 
+        for (int i = 0; i < dictionaryContent.size(); i++) {
+            String candidate = dictionaryContent.get(i);
+            int lengthSim = Math.abs(candidate.length() - word.length());
+            if (lengthSim > tolerance) {
+                continue;
+            }
+            double percent = helperCommonPercent(word, candidate);
+            if (percent < commonPercent) {
+                continue;
+            }
+            double sim = getSimilarity(word, candidate);
+            int insertIndex = helperGetIndex(similarNum, sim);
+            if (insertIndex < topN) {
+                bestSuggest.add(insertIndex, candidate);
+                similarNum.add(insertIndex, sim);
+                if (bestSuggest.size() > topN) {
+                    bestSuggest.remove(bestSuggest.size() - 1);
+                    similarNum.remove(similarNum.size() - 1);
+                }
+            }
+        }
 
-        return 0;
+        return bestSuggest;
     }
 
-    String s = "8,90,-7";
-    Scanner scnr = new Scanner(s).useDelimiter(",");
+    private double helperCommonPercent(String one, String two) {
+        HashSet<Character> setA = new HashSet<>();
+        for (int i = 0; i < one.length(); i++) {
+            setA.add(one.charAt(i));
+        }
+        HashSet<Character> setB = new HashSet<>();
+        for (int i = 0; i < two.length(); i++) {
+            setB.add(two.charAt(i));
+        }
+        HashSet<Character> intersection = new HashSet<>(setA);
+        intersection.retainAll(setB);
+        HashSet<Character> union = new HashSet<>(setA);
+        union.addAll(setB);
+        if (union.isEmpty()) {
+            return 0.0;
+        }
+        return (double) intersection.size() / union.size();
+    }
 
+    private int helperGetIndex(ArrayList<Double> scores, double sim) {
+        int i = 0;
+        while (i < scores.size() && sim <= scores.get(i)) {
+            i++;
+        }
+        return i;
+    }
 
     public double getSimilarity(String word1, String word2) {
         String shortWord;
@@ -60,19 +90,28 @@ public class WordRecommender {
         } else {
             shortWord = word2;
         }
-
         for (int i = 0; i < shortWord.length(); i++) {
             if (word1.charAt(i) == word2.charAt(i)) {
                 leftSim++;
             }
         }
         for (int i = 0; i < shortWord.length(); i++) {
-            if (word1.charAt(word1.length() - 1 - i) == word2.charAt(word2.length() - 1 - i)) {
+            if (word1.charAt(word1.length() - 1 - i) ==
+                    word2.charAt(word2.length() - 1 - i)) {
                 rightSim++;
             }
         }
         return (rightSim + leftSim) / 2.0;
-
-
     }
 }
+
+
+
+    // final String shortest = List.of("hello", "yes", "no")
+    //      .stream()
+    //       .min(Comparator.comparing(String::length))??
+    //      .get();
+
+//          for (int i = 0; i < minLength)
+
+
